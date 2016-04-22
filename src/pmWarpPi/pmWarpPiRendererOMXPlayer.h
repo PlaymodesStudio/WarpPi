cc303094306f872b9f9dc24383fed0f4675b9938
eduardfrigola <eduardfrigola@yahoo.com>
2016-04-22 12:31:42 +0200

    major change in omxplayer now inherits from videoPlayer, it's not working at the moment

--- src/pmWarpPi/pmWarpPiRendererOMXPlayer.h
+++ src/pmWarpPi/pmWarpPiRendererOMXPlayer.h
@@ -5,39 +5,71 @@
 #ifdef TARGET_RASPBERRY_PI
 
 #include "ofxOMXPlayer.h"
-#include "pmWarpPiRendererScreen.h"
+#include "pmWarpPiRendererVideoPlayer.h"
 #include "ofMain.h"
 
-class pmWarpPiRendererOMXPlayer : public pmWarpPiRendererScreen
+class pmWarpPiRendererOMXPlayer : public pmWarpPiRendererVideoPlayer
 {
     
 public:
  
-    pmWarpPiRendererOMXPlayer();
+    pmWarpPiRendererOMXPlayer(){pmWarpPiRendererVideoPlayer();};
     
-    virtual void        updateOSC(ofxOscMessage* m);
+    //override methods
+    virtual void createPlayer(){
+        videoPlayer = new ofxOMXPlayer();
+        ofLog(OF_LOG_NOTICE) << "pmOmxPlayer :: New OmxPlayer";
+    } override
     
-    virtual void        deleteRenderer();
-    virtual void        showDebug();
+    virtual void loadAndPlay(){
+        string videoPath = ofToDataPath(videoFileName, true);
+        //Somewhat like ofFboSettings we may have a lot of options so this is the current model
+        ofxOMXPlayerSettings settings;
+        settings.videoPath = videoPath;
+        settings.useHDMIForAudio	= false;		//default true
+        settings.enableTexture		= true;		//default true
+        settings.enableLooping		= true;		//default true
+        settings.enableAudio		= true;		//default true, save resources by disabling
+        //settings.doFlip     Texture = true;		//default false
+        
+        if (!settings.enableTexture)
+        {
+            /*
+             We have the option to pass in a rectangle
+             to be used for a non-textured player to use (as opposed to the default full screen)
+             */
+            settings.displayRect.width = videoSize.x;
+            settings.displayRect.height = videoSize.y;
+            settings.displayRect.x = 0;
+            settings.displayRect.y = 0;
+        }
+        //so either pass in the settings
+        videoPlayer->setup(settings);
+    } override
     
-    /// CLASS FUNCTIONS
-    ////////////////////
-    virtual void        updateForScreen();
-
-    virtual void        drawIntoFbo();
-    virtual void        setupOMXPlayer(string _name,ofVec2f _pos, ofVec2f _size);
-    virtual void        onComplete(float* arg);
-//    void                showVideoPlayerDebug();
-    void                testVideo(bool test);
+    virtual void playPlayer(){
+        setPlayerPaused(false);
+    }override
     
-    /// CLASS PARAMS
-    /////////////////
-    ofxOMXPlayer*       omxPlayer;
-    //~ ofxOMXPlayer	testPlayer;
-    string              videoFileName;
-    ofVec2f             videoPosition;
-    ofVec2f             videoSize;
-    ofVec2f             videoPlayerDebugPosition;
+    virtual void updatePlayer(){
+        //It auto updates
+    }override
+    
+    virtual void stopPlayer(){
+        setPlayerPaused(true);
+    }override
+    
+    virtual ofLoopType getPlayerLoopState(){
+        if(videoPlayer->isLoopingEnabled) return OF_LOOP_NORMAL;
+        else return OF_LOOP_NONE;
+    }override
+    
+    
+    
+private:
+    
+    
+    ofxOMXPlayer*       videoPlayer;
     
     
     
