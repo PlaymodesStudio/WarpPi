701643d0ee13582a4406442efaff0e282303e62c
Eduard Frigola <eduardfrigola@yahoo.com>
2016-05-04 11:15:20 +0200

    Added ofxArtNet_PM, added artnetRenderer

---
+++ src/pmWarpPi/pmWarpPiRendererArtNet.cpp
@@ -0,0 +1,9 @@
+//
+//  pmWarpPiRendererArtNet.cpp
+//  WarpPi
+//
+//  Created by Eduard Frigola on 04/05/16.
+//
+//
+
+#include "pmWarpPiRendererArtNet.h"
---
+++ src/pmWarpPi/pmWarpPiRendererArtNet.h
@@ -0,0 +1,15 @@
+//
+//  pmWarpPiRendererArtNet.h
+//  WarpPi
+//
+//  Created by Eduard Frigola on 04/05/16.
+//
+//
+
+#ifndef pmWarpPiRendererArtNet_h
+#define pmWarpPiRendererArtNet_h
+
+#include "ofMain.h"
+#include "ofxArtNet.h"
+
+#endif /* pmWarpPiRendererArtNet_h */
