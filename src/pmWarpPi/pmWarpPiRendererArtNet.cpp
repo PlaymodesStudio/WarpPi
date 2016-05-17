9e8df05d31d2406d7b4febbdcb787648087b03ef
eduardfrigola <eduardfrigola@yahoo.com>
2016-05-17 09:34:56 +0200

    Included ArtNet functions from ArtNet player, not implemented.

--- src/pmWarpPi/pmWarpPiRendererArtNet.cpp
+++ src/pmWarpPi/pmWarpPiRendererArtNet.cpp
@@ -7,3 +7,94 @@
 //
 
 #include "pmWarpPiRendererArtNet.h"
+
+bool pmWarpPiRendererArtNet::setMachineIP(string machineIP){
+    
+    artnet.init(machineIP);
+    if(function == PM_ARTNET_PLAYER){
+        artnet.setSubNet(0);
+        artnet.setPortType(0, ARTNET_PORT_ENABLE_INPUT, ARTNET_DATA_DMX);
+        artnet.setPortAddress(0, ARTNET_PORT_INPUT, 0);
+        
+    }else if(function == PM_ARTNET_RECORDER){
+        artnet.setShortName("Artnet receive");
+        artnet.setLongName("Artnet Mac receiver");
+        artnet.setNodeType(ARTNET_TYPE_NODE);
+        artnet.setSubNet(0);
+        artnet.setPortType(0, ARTNET_PORT_ENABLE_OUTPUT, ARTNET_DATA_DMX);
+        artnet.setPortAddress(0, ARTNET_PORT_OUTPUT, 0);
+        ofAddListener(artnet.dmxData, this, &pmWarpPiRendererArtNet::receiveData);
+    }
+    
+    ofAddListener(artnet.pollReply, this, &pmWarpPiRendererArtNet::receivePollReply);
+    artnet.sendPoll();
+    start();
+}
+
+bool pmWarpPiRendererArtNet::setup(pmArtnetFunction _function)
+{
+    function = _function;
+    bStarted = false;
+    //    if(function == PM_ARTNET_PLAYER){
+    //        artnet.setSubNet(0);
+    //        artnet.setPortType(0, ARTNET_PORT_ENABLE_INPUT, ARTNET_DATA_DMX);
+    //        artnet.setPortAddress(0, ARTNET_PORT_INPUT, 0);
+    //
+    //    }else{
+    //        artnet.setShortName("Artnet receive");
+    //        artnet.setLongName("Artnet Mac receiver");
+    //        artnet.setNodeType(ARTNET_TYPE_NODE);
+    //        artnet.setSubNet(0);
+    //        artnet.setPortType(0, ARTNET_PORT_ENABLE_OUTPUT, ARTNET_DATA_DMX);
+    //        artnet.setPortAddress(0, ARTNET_PORT_OUTPUT, 0);
+    //        ofAddListener(artnet.dmxData, this, &pmWarpPiRendererArtNet::receiveData);
+    //    }
+}
+
+void pmWarpPiRendererArtNet::start(){
+    artnet.start();
+    bStarted = true;
+}
+
+void pmWarpPiRendererArtNet::setFromPixels(ofPixels &pixels){
+    for (int i = 0; i < pixels.getHeight(); i++){
+        for ( int j = 0; j < pixels.getLine(i).getStride()-1; j++){
+            linepixels[j] = (pixels.getLine(i).asPixels()[j]);
+        }
+        dmxDataPacket[i].setData(linepixels);
+        //dmxDataPacket[i].setPort(i);
+    }
+}
+
+bool pmWarpPiRendererArtNet::sendDmx(){
+    for (auto dmxUniverse : dmxDataPacket){
+        artnet.sendDmx(dmxUniverse);
+        cout<<dmxUniverse.getIp()<<endl;
+    }
+}
+
+bool pmWarpPiRendererArtNet::sendDmx(ofPixels &pixels){
+    setFromPixels(pixels);
+    sendDmx();
+}
+
+void pmWarpPiRendererArtNet::setUniverses(int universes){
+    nUniverses = universes;
+    for(int i = 0 ; i<nUniverses ; i++){
+        ofxArtNetDmxData tempPacket;
+        tempPacket.setPort(0);
+        vector<unsigned char> dataVec;
+        dataVec.assign(512, sizeof(unsigned char));
+        tempPacket.setData(dataVec);
+        dmxDataPacket.push_back(tempPacket);
+    }
+    linepixels.assign(512, sizeof(unsigned char));
+}
+
+void pmWarpPiRendererArtNet::receivePollReply(ofxArtNetNodeEntry &node){
+    string ipOfNode = node.getIp();
+    ofNotifyEvent(receivedNode,ipOfNode);
+    int univi = node.getUniverseOutput(0);
+    univi = univi>>14;
+    cout<<"NODE FOUND: "<<node.getIp()<< "  -  Subnet "<<node.getSubnet()<<"  Universe "<<node.getShortName()<<endl;
+}
--- src/pmWarpPi/pmWarpPiRendererArtNet.h
+++ src/pmWarpPi/pmWarpPiRendererArtNet.h
@@ -9,7 +9,49 @@
 #ifndef pmWarpPiRendererArtNet_h
 #define pmWarpPiRendererArtNet_h
 
-#include "ofMain.h"
+#include "pmWarpPi.h"
 #include "ofxArtNet.h"
+#include "pmWarpPiRendererVideoPlayer.h"
+
+enum pmArtnetFunction {
+    PM_ARTNET_RECORDER,
+    PM_ARTNET_PLAYER
+};
+
+class pmWarpPiRendererArtNet{
+    
+public:
+    pmWarpPiRendererArtNet(){};
+    ~pmWarpPiRendererArtNet(){};
+    
+    bool setMachineIP(string machineIP);
+    bool setup(pmArtnetFunction _function);
+    void start();
+    void setFromPixels(ofPixels &pixels);
+    bool sendDmx();
+    bool sendDmx(ofPixels &pixels);
+    void setTargetIP(string ip, int line){dmxDataPacket[line].setIp(ip);};
+    void setTargetSubNet(int subnet, int line){dmxDataPacket[line].setSubNet(subnet);};
+    void setTargetUniverse(int universe, int line){dmxDataPacket[line].setUniverse(universe);};
+    vector<unsigned char> getData(int universe){return dmxDataPacket[universe].getData();};
+    void receiveData(ofxArtNetDmxData &data){dmxDataPacket[data.getPort()] = data;};
+    void receivePollReply(ofxArtNetNodeEntry &node);
+    bool isStarted(){return bStarted;};
+    vector<pair<string, string>> getIfacesIps(){return artnet.getIfacesIps();};
+    
+    int getUniverses(){return dmxDataPacket.size();};
+    void setUniverses(int universes);
+    ofEvent<string> receivedNode;
+    
+private:
+    bool bStarted;
+    pmArtnetFunction function;
+    ofxArtNet artnet;
+    int nUniverses;
+    vector<ofxArtNetDmxData> dmxDataPacket;
+    vector<unsigned char> linepixels;
+    
+};
+
 
 #endif /* pmWarpPiRendererArtNet_h */
